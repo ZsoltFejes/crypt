03b92a19fea8806054fcb696b44a78acc93e05c2
Zsolt Fejes <zsolt.fejes@icloud.com>
2020-09-20 11:28:45 +0000

    Added installation script

--- install.sh
+++ install.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-curl 
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+curl https://raw.githubusercontent.com/ZsoltFejes/crypt/master/dist/crypt --output /sbin/crypt
+chmod +x /sbin/crypt
+
+echo
+if ! command -v crypt &> /dev/null
+then
+	echo "crypt could not be found"
+	exit
+else
+	echo "Installation succesful"
+fi
+
+crypt -v
